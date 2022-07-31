75fea3da3ce272c90fb71142c29ce2a3c6dd3bad
Aya Ali <1aya.dn.ali@gmail.com>
2022-07-31 20:36:48 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+let number1 = 2;
+let number2 = 31;
+let multiply = number1 * number2;
+let random = Math.floor( Math.random() * 1000 ) + 1;
+const number3 = 16;
+const number4 = 6;
+const mod = number3 % number4;
+const max = Math.max( 5, 10, 15, 20 );
+
